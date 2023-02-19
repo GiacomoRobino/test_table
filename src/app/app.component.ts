fd9ea37a0198d696897e5b9e4ffd3ceb57c6ed85
GiacomoRobino <robinogiacomo@gmail.com>
2023-02-19 21:17:37 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { DataService } from './services/data-service/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test';
+  dataSource$ !: Observable<any>;
+  constructor(public dataService: DataService){}
+  ngOnInit(){
+    this.dataSource$ = this.dataService.getDataStream();
+    this.dataService.getData();
+  }
 }
